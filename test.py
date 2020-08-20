98fbaf691fe9dc432330d9d8969ea8d05ade8d6b
Mabel Le <mabelle.ky@gmail.com>
2020-08-20 00:11:59 -0400

    data cleaning script for salary estimates only

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 """
-Created on Sat Aug 17  05:28:31 2020
+Created on Sat Aug 17  17:28:31 2020
 
 @author: mabelleky
 """
@@ -9,7 +9,8 @@ import pandas as pd
 
 path = "D:/DS_Projects/data_science_salary_predictor/chromedriver.exe"
 
-pd.set_option("display.max_columns", None)
+pd.set_option("display.max_columns", None) # to display all columns in Pycharm
 dataFrame = gs.get_jobs("data scientist", 1000, False, path, 15)
 dataFrame.to_csv("glassdoor_jobs.csv", index=False)
-print(dataFrame)
+dataFrame
+# for PyCharm IDE use print(dataFrame)
